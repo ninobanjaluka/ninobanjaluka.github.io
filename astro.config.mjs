b1f04771ad6a3446d1f0f9f66a5ef263f8e826ce
ninobanjaluka <mycoperson@gmail.com>
2026-05-30 12:54:07 +0200

    chore: migrate deployment config to ninobanjaluka account

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: 'https://katnino.github.io',
-  trailingSlash: 'never',
+  site: "https://ninobanjaluka.github.io",
+  trailingSlash: "never",
   integrations: [mdx(), sitemap()],
   markdown: {
     shikiConfig: {
-      theme: 'github-dark',
+      theme: "github-dark",
       wrap: true,
     },
   },
